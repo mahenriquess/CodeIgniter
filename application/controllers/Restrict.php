bab8b3b75f1031f9f64fb5bc98eb0050b241bb9a
mahenriquess <mahenriquess@outlook.com>
2019-09-03 16:16:36 -0300

    primeiro commit - template customizado e controler para area restrita ok

---
+++ application/controllers/Restrict.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Restrict extends CI_Controller{
+
+	public function index(){
+		$this->template->show("login.php");
+	}
+
+}
---
+++ application/views/login.php
@@ -0,0 +1,20 @@
+		<section style="min-height: calc(100vh - 83px);" class="light-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<div class="section-title">
+							<h2>LOGIN</h2>
+
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<!-- team member item -->
+					<div class="col-md-4">
+						<div class="team-item">
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
